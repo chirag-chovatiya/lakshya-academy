fb14b43e8d9bba70a5c37d7c4b50c7089d741532
chirag-chovatiya <134358879+chirag-chovatiya@users.noreply.github.com>
2025-01-22 08:08:57 +0530

    some ui change and timer set

--- src/app/home/components/test-result.jsx
+++ src/app/home/components/test-result.jsx
@@ -1,31 +1,50 @@
-const Results = ({ questions, userAnswers, questionType, getSign, totalScore }) => {
-    return (
-      <div>
-        <h2 className="text-xl font-semibold py-2  sm:text-left">Total Score: {totalScore}</h2>
-        <ul>
-          {questions.map((q, index) => {
-            const userAnswer = userAnswers[index]?.userAnswer || null;
-            const isCorrect = userAnswer === q.answer;
-            return (
-              <li key={index} className="mt-2">
-                <p className="text-3xl font-semibold">
-                  Q {index + 1}. {q.question.join(" " + getSign(questionType) + " ")}
-                </p>
-                <div className="flex flex-col sm:flex-row justify-between md:items-center text-xl py-2">
-                  <span className="text-green-500 mb-2 sm:mb-0">
-                    <strong>Correct Answer:</strong> {q.answer}
-                  </span>
-                  <span className={`font-semibold ${isCorrect ? "text-green-500" : "text-red"}`}>
-                    <strong>Your Answer:</strong> {userAnswer} {isCorrect ? "(Correct)" : "(Incorrect)"}
-                  </span>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+const Results = ({
+  questions,
+  userAnswers,
+  questionType,
+  getSign,
+  totalScore,
+  elapsedTime,
+}) => {
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <h2 className="text-xl font-semibold py-2  sm:text-left">
+          Total Score: {totalScore}
+        </h2>
+        <h3 className="text-lg text-gray-700 py-2">
+          Time Taken: {elapsedTime}
+        </h3>
       </div>
-    );
-  };
-  
-  export default Results;
-  
+      <ul>
+        {questions.map((q, index) => {
+          const userAnswer = userAnswers[index]?.userAnswer || null;
+          const isCorrect = userAnswer === q.answer;
+          return (
+            <li key={index} className="mt-2">
+              <p className="text-3xl font-semibold">
+                Q {index + 1}.{" "}
+                {q.question.join(" " + getSign(questionType) + " ")}
+              </p>
+              <div className="flex flex-col sm:flex-row justify-between md:items-center text-xl py-2">
+                <span className="text-green-500 mb-2 sm:mb-0">
+                  <strong>Correct Answer:</strong> {q.answer}
+                </span>
+                <span
+                  className={`font-semibold ${
+                    isCorrect ? "text-green-500" : "text-red"
+                  }`}
+                >
+                  <strong>Your Answer:</strong> {userAnswer}{" "}
+                  {isCorrect ? "(Correct)" : "(Incorrect)"}
+                </span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Results;

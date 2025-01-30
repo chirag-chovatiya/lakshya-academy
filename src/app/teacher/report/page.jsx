b583c7d4fbaf9147f2bf1c8699d6cec0139b1f55
chirag-chovatiya <134358879+chirag-chovatiya@users.noreply.github.com>
2025-01-30 21:44:18 +0530

    student month report filter and pagination proper set

--- src/app/teacher/report/page.jsx
+++ src/app/teacher/report/page.jsx
@@ -174,8 +174,8 @@ export default function StudentLists() {
               <input
                 type="date"
                 id="dateSearch"
-                value={createdAt}
                 className="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white outline-none"
+                value={createdAt}
                 onChange={(e) => setCreatedAt(e.target.value)}
               />
             </div>
